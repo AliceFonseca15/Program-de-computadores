8d5ed6c1ecdad78c5210bdbac48d663eb8f1580c
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2025-02-05 22:58:20 -0300

    OBI.py
    teste

---
+++ codeforces/lista10/OBI.py
@@ -0,0 +1,9 @@
+jogadores, min_pontos = map(int,input().split())
+passou = []
+
+  for i in range(0,jogadores):
+      pontos1, pontos2 = map(int,input().split())
+      if pontos1 + pontos2 >= min_pontos:
+          passou.append(1)
+     
+print(sum(passou))
