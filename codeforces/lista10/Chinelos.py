6395ab739f143b12ed640489a57ae1d9777c681b
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2025-02-19 14:23:31 -0300

    Update Chinelos.py
    inacabado

--- codeforces/lista10/Chinelos.py
+++ codeforces/lista10/Chinelos.py
@@ -10,23 +10,17 @@ pedidos = int(input())
 tam = []
 for i in range(pedidos):
     tamanho = int(input())
-
+    tam.append(tamanho)
 
 
 vendidos = 0
-for i in range(len(quantidades)):
-    for x in range(len(pedidos)):
-        if quantidades[x - 1] > 0:
-            menos = i - 1
-            quantidades.insert(i,menos)
-            vendidos += 1
+for i in range(len(tam)):
+    for j in range(len(quantidades)):
+        indice = tam[i] - 1
+        if j == indice:
+            if tam[i] != 0:
+                quantidades[j] = quantidades[j] - 1
+                vendidos = vendidos + 1
+
         
-
-#for i in range(0,len(quantidades)):
- # for x in range(0,pedidos):
- #     if i == x:
- #         menos = i - 1
- #         vendidos = vendidos + 1
- #         quantidades.insert(i,menos)
-
 print(vendidos)
