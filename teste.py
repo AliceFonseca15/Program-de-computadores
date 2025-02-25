93712dc3794c2924e56b2f7a4e3c4e45807f58be
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2025-02-25 08:22:32 -0300

    .

--- teste.py
+++ teste.py
@@ -1,7 +1,8 @@
-pedidos = [1,3]
-quantidades = [4,2,0,3,2]
-new_quant = []
-while pedidos != []:
-    if quantidades[pedidos[-1] - 1] > 0:
-        novo = 
-  
+if a > c and a > g and a > t:
+    print(a)
+    if c > a and c > g and c > t:
+        print(c)
+        if g > a and g > c and g > t:
+            print(g)
+            if t > a and t > c and t > g:
+                print(t)
