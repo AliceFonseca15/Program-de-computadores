ff0a559990f482fdfff144eaf3d65aea0f530674
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2025-02-25 15:48:02 -0300

    limpar

--- teste.py
+++ teste.py
@@ -1,8 +1,1 @@
-if a > c and a > g and a > t:
-    print(a)
-    if c > a and c > g and c > t:
-        print(c)
-        if g > a and g > c and g > t:
-            print(g)
-            if t > a and t > c and t > g:
-                print(t)
+
