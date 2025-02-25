d15e885834209cfa14f041d3b69876d4c3ec34d7
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2025-02-25 15:47:33 -0300

    Update repetição.py

--- codeforces/lista10/repetição.py
+++ codeforces/lista10/repetição.py
@@ -1,21 +1,64 @@
-sequencia = (input())
+sec = (input())
 
 a = 0
 c = 0
 g = 0
 t = 0
 
-sec = list(map(str, sequencia)) 
+var_a = 1
+var_c = 1
+var_g = 1
+var_t = 1
+if len(sec) == 1:
+    print("1")
+else:
+    for i in range(1, len(sec)):
+        if sec[i] == "A":
+            if sec[i] == sec[i - 1]:
+                var_a += 1
+            else:
+                var_a = 1
+            a = max(a, var_a)
+         
+        if sec[i] == "C":
+            if sec[i] == sec[i - 1]:
+                var_c += 1
+            else:
+                var_c = 1
+            c = max(c, var_c)
 
-for i in sequencia:
-    if i == "A" or i == "a":
-        a = a + 1
-    if i == "C" or i == "c":
-        c = c + 1
-    if i == "G" or i == "g" :
-        g = g + 1
-    if i == "T" or i == "t":
-        t = t + 1
+        if sec[i] == "G": 
+            if sec[i] == sec[i - 1]:
+                var_g += 1
+            else:
+                var_g = 1
+            g = max(g, var_g)
 
-numero = [a,c,g,t]
-print(max(numero))
+        if sec[i] == "T":
+            if sec[i] == sec[i - 1]:
+                var_t += 1
+            else:
+                var_t = 1
+            t = max(t, var_t)
+
+
+    numero = max(a, c, g, t)
+
+    print(numero)
+
+    
+
+
+
+
+
+    
+    
+    
+
+        
+
+
+
+
+    
